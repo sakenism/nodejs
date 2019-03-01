19515489079fe6b12e6ce5528f541d300c6b9acb
Saken Mukanov <boss@MacBook-Air-Saken.local>
2019-03-01 13:19:16 +0600

    commit

---
+++ server-request-body.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const port = 3000;
+http.createServer((req, res) => {
+	console.log(req.headers);
+	console.log(req.method);
+	console.log(req.statusCode);
+	console.log(req.url);
+	if (req.method == 'POST') {
+		let buff = ''
+		req.on('data', (chunk) => {
+			buff += chunk;
+		})
+		req.on('end', () => {
+			console.log('Body: ' + buff);
+			res.end('\nAccepted body\n');
+		});
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end('Hello World\n');
+	}
+}).listen(port);
